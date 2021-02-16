e75ebb908487aeded4678b040d3aeff7e2f7fca2
jonathans199 <jso1919@gmail.com>
2021-02-16 13:22:40 -0500

    updated readme

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> This is jons portfolio sssss new changes
+          Edit <code>src/App.js</code> This is my react app
         </p>
         <a
           className="App-link"
