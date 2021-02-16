ac2c4dc58429d4b7cb339e5fdd5132c1d4f3383b
jonathans199 <jso1919@gmail.com>
2021-02-16 12:48:33 -0500

    updated home page with new changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> This is jons portfolio sssss
+          Edit <code>src/App.js</code> This is jons portfolio sssss new changes
         </p>
         <a
           className="App-link"
